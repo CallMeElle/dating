86be880e70b1f42dbcd694ab5b875791eca00201
CallMeElle <joelle@j-hohwiller.de>
2021-05-30 22:12:31 +0200

    test PHP seite

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <title>Dating App</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="site.css">
+</head>
+<body>
+<h1>Dating App</h1>
+Hello World.
+<br>
+<?php echo date("Y-m-d") ?>
+</body>
+</html>
+ 
