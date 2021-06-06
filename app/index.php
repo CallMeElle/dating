85e019245bb27fc4628a3b42f4f8eab65731a911
elle <joelle@j-hohwiller.de>
2021-06-06 21:21:25 +0200

    fixed index.php

--- app/index.php
+++ app/index.php
@@ -1,4 +1,18 @@
-<?php include('head.php');?>
-Willkommen, wie können wir Dir helfen?..
-<a href="neuernutzer.php">hier gehts zur Anmeldung</a>
-<?php include('foot.php');?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<?
+    $head_file = file_get_contents("html/head.html");
+    echo $head_file;
+?>
+
+<html>
+    <body>
+        Willkommen, wie können wir Dir helfen?..
+        <a href="neuernutzer.php">hier gehts zur Anmeldung</a>
+    </body>
+</html>
+
+<?php 
+    $foot_file = file_get_contents("html/foot.html");
+    echo $foot_file;
+?>
