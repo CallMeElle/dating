5f86d7db293891c6a394bd4ff7a4929caa7e50dc
CallMeElle <joelle@j-hohwiller.de>
2021-06-13 13:59:23 +0200

    Überarbeitet Neuernutzer

--- app/neuernutzer2.php
+++ app/neuernutzer2.php
@@ -4,19 +4,35 @@
     
     include_once("account_management.php");
     
-    include_once('head.php');
+    $head_file = file_get_contents("html/head.html");
+    echo $head_file;
+?>
 
-    $Username = $_POST["register_Username"];
-    $Passwort = $_POST["register_Passwort"];
-    $Email = $_POST["register_Email"];
-    
-    $err = register($Username, $Passwort, $Email);
-    
-    if($err){
-        echo "Fehler bei der Anmeldung";
-    }
-    
-    
-    include_once('foot.php');
+
+<html>
+    <body>
+        <div class="main">
+
+            <?php
+                $Username = $_POST["register_Username"];
+                $Passwort = $_POST["register_Passwort"];
+                $Email = $_POST["register_Email"];
+                
+                $err = register($Username, $Passwort, $Email);
+                
+                if($err){
+                    echo "Fehler bei der Anmeldung";
+                }else{
+                    echo "Neuer nutzer erfolgreich erstellt";
+                }
+            ?>    
+            
+        </div>
+    </body>
+</html>
+  
+<?php  
+    $foot_file = file_get_contents("html/foot.html");
+    echo $foot_file;
     
 ?>
