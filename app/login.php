601c70d79eb4d686afeba651a7cb39c3ef7829e6
CallMeElle <joelle@j-hohwiller.de>
2021-06-17 21:08:47 +0200

    alle HTML ausgaben raus

--- app/login.php
+++ app/login.php
@@ -1,15 +1,6 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-
 <?php
+    error_reporting(E_ERROR | E_PARSE);
     include_once "Funktionen/account_management.php";
-?>
-
-
-<html>
-    <body>
-        <div class="main">
-
-            <?php
                 $Username = $_POST["Username"];
                 $Passwort = $_POST["Passwort"];
                
@@ -22,14 +13,4 @@
                     die();
                 }
 
-            ?>    
-            
-        </div>
-    </body>
-</html>
-  
-<?php  
-    $foot_file = file_get_contents("html/foot.html");
-    echo $foot_file;
-    
 ?>
