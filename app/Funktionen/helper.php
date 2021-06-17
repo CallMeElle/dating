c758ab23d729789c759914de6841951696189ff3
CallMeElle <joelle@j-hohwiller.de>
2021-06-17 21:12:37 +0200

    kein HTML aus Funktionen

--- app/Funktionen/helper.php
+++ app/Funktionen/helper.php
@@ -1,7 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-
 <?php
-
     function printJSDebug($data){
         $is_debug = true;
         
@@ -10,6 +7,5 @@
             echo "    console.log('$data');";
             echo "</script>";
         }
-    }
-    
+    }  
 ?>
--- app/Funktionen/session.php
+++ app/Funktionen/session.php
@@ -1,6 +1,3 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-
-
 <?php
 
     include_once "helper.php";
@@ -84,6 +81,7 @@
 
         //switch visibility for logout button here @todo
         
+        
         return true;
     }
 
@@ -94,4 +92,3 @@
     }
     
 ?>
-
